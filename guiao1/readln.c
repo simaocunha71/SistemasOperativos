fd51e38df29d3ea458f160598a989c919ee88465
Simão Pedro Sá Cunha <61991247+simaocunha71@users.noreply.github.com>
2022-02-14 18:41:00 +0000

    upload guioes

---
+++ guiao1/readln.c
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+#include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
+#include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
