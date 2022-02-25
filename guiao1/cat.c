8ed168547da86545d971e2654e771a81bc946d94
simaocunha71 <a93262@alunos.uminho.pt>
2022-02-25 00:50:53 +0000

    commit de teste VM

--- guiao1/cat.c
+++ guiao1/cat.c
@@ -2,8 +2,7 @@
 #include <unistd.h> /* chamadas ao sistema: defs e decls essenciais */
 #include <fcntl.h> /* O_RDONLY, O_WRONLY, O_CREAT, O_* */
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     /* code */
     return 0;
 }
